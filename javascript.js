bbae7612bd1d906e8d5532ecddc3a2a13e75caf2
Aermyx <54551414+Aermyx@users.noreply.github.com>
2020-09-11 09:32:04 -0400

    Update javascript.js

--- javascript.js
+++
@@ -1,6 +0,0 @@
-const jouer = document.querySelector(".jouer");
-
-jouer.addEventListener("click", () => {
-    document.querySelector(".boxFormulaire").classList.add("none");
-    document.querySelector(".fondFormulaire").classList.add("none");
-})
